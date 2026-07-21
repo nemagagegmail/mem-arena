bc59d3dce01b346445abb9e15b4198b3cf8d6e5b
nemagagegmail <nemagage.y@gmail.com>
2026-07-21 19:32:21 +0300

    mem_scope.h: add force-inline for consistency

--- mem_scope.h
+++ mem_scope.h
@@ -11,7 +11,7 @@ typedef struct {
 /* 
  * Enters a temporary memory scope, saving the current arena offset.
  */
-static inline mem_scope_t mem_scope_begin(mem_arena_t *arena) {
+MEM_FORCE_INLINE mem_scope_t mem_scope_begin(mem_arena_t *arena) {
     mem_scope_t scope;
     scope.arena = arena;
     scope.saved_offset = arena->offset;
@@ -22,7 +22,7 @@ static inline mem_scope_t mem_scope_begin(mem_arena_t *arena) {
  * Exits the scope, automatically restoring the arena offset 
  * and freeing all memory allocated within this scope.
  */
-static inline void mem_scope_end(mem_scope_t *scope) {
+MEM_FORCE_INLINE void mem_scope_end(mem_scope_t *scope) {
     scope->arena->offset = scope->saved_offset;
 }
 
